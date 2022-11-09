86edc49b6106ef808f08c3dacfe002febfeb222e
Fannie Persson <fannie.persson@consid.se>
2022-11-09 11:45:49 +0100

    greetings

---
+++ FannieExercise/IGreeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FannieExercise
+{
+    internal interface IGreeting
+    {
+        public enum Greetings
+        {
+            Hello, Goodbye
+        }
+
+        public void SayHello();
+        public string SendGreeting(Greetings greetings);
+    }
+}
